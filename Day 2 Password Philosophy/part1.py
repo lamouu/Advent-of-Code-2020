ab86d565a67891fa66c648a9c48549625986d586
lamouu <69391156+lamouu@users.noreply.github.com>
2020-12-02 15:06:41 +0000

    Day 2 part 2 first attempt

--- Day 2 Password Philosophy/part1.py
+++ Day 2 Password Philosophy/part1.py
@@ -5,7 +5,7 @@ with open('input.txt') as f:
 
 valid = 0
 for line in data:
-    if int(line.split(" ", 1)[0].split("-", 1)[1]) >= int(line.split(':',1)[1].count(line[line.find(':') - 1])) >= int(line.split(" ", 1)[0].split("-", 1)[0]):
+    if int(line.split(" ", 1)[0].split("-", 1)[1]) >= int(line.split(': ',1)[1].count(line[line.find(':') - 1])) >= int(line.split(" ", 1)[0].split("-", 1)[0]):
         valid += 1
 
 print(f"{valid} valid passwords")
--- Day 2 Password Philosophy/part2.py
+++ Day 2 Password Philosophy/part2.py
@@ -5,12 +5,14 @@ with open('input.txt') as f:
 
 valid = 0
 for line in data:
-    password = line.split(':',1)[1]
-    count = line.split(':',1)[1].count(line[line.find(':') - 1])
-    maximum = line.split(" ", 1)[0].split("-", 1)[1]
-    minimum = line.split(" ", 1)[0].split("-", 1)[0]
+    print("Line:",line)
+    print("Password:",line.split(': ',1)[1])
+    print("Index 1:",int(line.split(" ", 1)[0].split("-", 1)[0]) - 1)
+    print("Index 2:",int(line.split(" ", 1)[0].split("-", 1)[1]) - 1)
+    print("Character:",line[line.find(':') - 1])
+    print("\n")
 
-    if int(line.split(" ", 1)[0].split("-", 1)[1]) >= int(line.split(':',1)[1].count(line[line.find(':') - 1])) >= int(line.split(" ", 1)[0].split("-", 1)[0]):
+    if line.split(': ',1)[1][int(line.split(" ", 1)[0].split("-", 1)[0]) - 1] == line[line.find(':') - 1] or line.split(': ',1)[1][int(line.split(" ", 1)[0].split("-", 1)[1]) - 1] == line[line.find(':') - 1]:
         valid += 1
 
 print(f"{valid} valid passwords")
