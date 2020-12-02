ee3ff326c89385635c0e04a6533a4a6b64aedbdd
lamouu <69391156+lamouu@users.noreply.github.com>
2020-12-02 22:56:57 +0000

    Day 2 Part 2

--- Day 2 Password Philosophy/part2.py
+++ Day 2 Password Philosophy/part2.py
@@ -5,14 +5,7 @@ with open('input.txt') as f:
 
 valid = 0
 for line in data:
-    print("Line:",line)
-    print("Password:",line.split(': ',1)[1])
-    print("Index 1:",int(line.split(" ", 1)[0].split("-", 1)[0]) - 1)
-    print("Index 2:",int(line.split(" ", 1)[0].split("-", 1)[1]) - 1)
-    print("Character:",line[line.find(':') - 1])
-    print("\n")
-
-    if line.split(': ',1)[1][int(line.split(" ", 1)[0].split("-", 1)[0]) - 1] == line[line.find(':') - 1] or line.split(': ',1)[1][int(line.split(" ", 1)[0].split("-", 1)[1]) - 1] == line[line.find(':') - 1]:
+    if (line.split(': ',1)[1][int(line.split(" ", 1)[0].split("-", 1)[0]) - 1] == line[line.find(':') - 1] and line.split(': ',1)[1][int(line.split(" ", 1)[0].split("-", 1)[1]) - 1] != line[line.find(':') - 1]) or (line.split(': ',1)[1][int(line.split(" ", 1)[0].split("-", 1)[0]) - 1] != line[line.find(':') - 1] and line.split(': ',1)[1][int(line.split(" ", 1)[0].split("-", 1)[1]) - 1] == line[line.find(':') - 1]):
         valid += 1
 
 print(f"{valid} valid passwords")
