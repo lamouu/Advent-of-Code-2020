1c842c5776bbce75440b14bb3aebfcd0073ec445
lamouu <69391156+lamouu@users.noreply.github.com>
2020-12-06 01:03:18 +0000

    Removed useless variables

--- Day 5 Binary Boarding/part1.py
+++ Day 5 Binary Boarding/part1.py
@@ -16,9 +16,7 @@ for seat in data:
         if seat[index] == "L":
             column = column[:len(column)//2]
 
-    rowIndex = row[0] - 1
-    columnIndex = column[0] - 1
-    id_ = rowIndex * 8 + columnIndex
+    id_ = (row[0] - 1) * 8 + column[0] - 1
 
     if id_ > highestId:
         highestId = id_
--- Day 5 Binary Boarding/part2.py
+++ Day 5 Binary Boarding/part2.py
@@ -16,14 +16,11 @@ for seat in data:
         if seat[index] == "L":
             column = column[:len(column)//2]
 
-    rowIndex = row[0] - 1
-    columnIndex = column[0] - 1
-    seatMatrix[rowIndex][columnIndex] = 1
+    seatMatrix[row[0] - 1][column[0] - 1] = 1
 
 for column in range(8):
     for row in range(128):
         if seatMatrix[row][column] == 0 and seatMatrix[row - 1][column] == 1  and seatMatrix[row + 1][column] == 1:
-            # search doesn't work for seats on the end of aisles
             id_ = row * 8 + column
 
 print(f"Your seat ID is {id_}.")
